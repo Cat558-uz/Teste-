d075e6a6e35eff3978b9e745394d014723159079
Profile <samuelfirmino802@gmail.com>
2025-04-07 16:11:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-function salvarTexto() {
-  const texto = document.getElementById("inputText").value;
-  localStorage.setItem("meuTexto", texto);
-  mostrarTexto();
-}
-
-function mostrarTexto() {
-  const textoSalvo = localStorage.getItem("meuTexto");
-  document.getElementById("textoSalvo").innerText = textoSalvo || "Nada salvo ainda.";
-}
-
-// Mostra automaticamente ao abrir a página
-mostrarTexto();
+fetch("https://sua-api.repl.co/api/youtube?q=lofi")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.results); // exibe os vídeos
+  });
